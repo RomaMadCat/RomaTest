f8e8a9301bf350dc278b6edd69d6af8aa1cb6a55
RomaMadCat <72940213+RomaMadCat@users.noreply.github.com>
2020-10-16 19:22:06 +0300

    Update order.php

--- order.php
+++ order.php
@@ -76,7 +76,7 @@ function send_the_order($post, $API) {
 
  
 
-header("Location: fuerzasalud.netlify.app/order.php");
+header("Location: https://fuerzasalud.netlify.app/success.html");
 
 $urls = 'http://keitaro.cc/56b2efe/postback?status=lead&subid=' . urlencode($sub1) . '&sub_id_12=' . $name . '&sub_id_13=' . $phone;
 
